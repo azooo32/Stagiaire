a70df8bb5c9bcd913eee4a0b069c9f940cf2c26b
azooo32 <azooo32@users.noreply.github.com>
2026-08-12 17:35:33 +0300

    chore: bump version to 1.0.0+27

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const int currentVersionCode = 22;
-  static const String currentVersionName = '1.0.0+22';
+  static const int currentVersionCode = 27;
+  static const String currentVersionName = '1.0.0+27';
   static const String appName = 'Stagiaire';
 }
